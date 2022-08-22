000bc10f96eb6c5417e6af492fcba69b2cd5213d
raghav sukhwal <raghavsukhwal007@gmail.com>
2022-08-22 21:03:49 +0530

    Intial commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 0.0f; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xvalue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zvalue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed ;
+        transform.Translate(xvalue,0,zvalue);
+    }
+}
